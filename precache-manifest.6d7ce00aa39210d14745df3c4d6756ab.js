210989627d0e7701eaaae617907058c043970a82
Govind <govind@rollinglogs.com>
2020-03-03 15:54:14 +0530

    Updates

---
+++ precache-manifest.6d7ce00aa39210d14745df3c4d6756ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822a8747f1f98ad710a5748b6e9a130c",
+    "url": "/puzzle/index.html"
+  },
+  {
+    "revision": "496a89a5b4cb2f562cfd",
+    "url": "/puzzle/static/css/main.97fc87e1.chunk.css"
+  },
+  {
+    "revision": "9f6370eae15b84055c6f",
+    "url": "/puzzle/static/js/2.ab5e77bc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/puzzle/static/js/2.ab5e77bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496a89a5b4cb2f562cfd",
+    "url": "/puzzle/static/js/main.bb0c0a12.chunk.js"
+  },
+  {
+    "revision": "c452104afac89e44369c",
+    "url": "/puzzle/static/js/runtime-main.0a398a58.js"
+  }
+]);
